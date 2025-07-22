f4b830fc4926390174567c05cc66da90e328a95b
duvan51 <duvanaponteramirez@gmail.com>
2025-07-21 22:37:50 -0500

    arregolado lo del consumo de las api

--- apis/api.js
+++ apis/api.js
@@ -10,9 +10,9 @@ import axios from "axios";
 //const CloudPhp = "http://10.0.2.2:8000/api"  //-> enmulador android
 //const CloudPhp = "http://127.0.0.1:8000/api";  //-> web android
 
-//const CloudPhp = "http://api.musikoord.com/api";
+const CloudPhp = "https://api.musikoord.com/api";
 
-const CloudPhp = "http://82.25.90.180:8000/api"; //-> web android
+//const CloudPhp = "http://82.25.90.180:8000/api"; //-> web android
 
 export const getSongs = async () => {
   try {
